4717d2c94a0211ccba6ddd777296734856ed66ec
Ayoub <ayoubjnieh@gmail.com>
2026-01-04 13:47:09 +0100

    Ajout code Firebase (services, repositories, models)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,23 @@
 plugins {
     alias(libs.plugins.android.application)
+    // AJOUT 1 : Le plugin Google Services pour Firebase
+    id("com.google.gms.google-services")
 }
 
 android {
+    // Vérifie que c'est bien ton nom de package ici
     namespace = "com.example.freelance"
+
+    // 👇 CORRECTION : On passe de 34 à 36
     compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.freelance"
-        minSdk = 25
+        minSdk = 24
+
+        // 👇 CORRECTION : On passe de 34 à 36
         targetSdk = 36
+
         versionCode = 1
         versionName = "1.0"
 
@@ -42,4 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // AJOUT 2 : Les dépendances Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-firestore")
 }
--- app/src/main/java/com/example/freelance/MainActivity.java
+++ app/src/main/java/com/example/freelance/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.freelance;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
